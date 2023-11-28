4a8d7ccce2b4d99761a876bae30393e8c84c6967
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-28 14:43:54 -0500

    Fixed: If a number is divisible by 3 and 5, log <<Fizz Buzz>> Added: If a number is not divisible by either 3 or 5, log the number.

--- practicalloops.js
+++ practicalloops.js
@@ -1,19 +1,21 @@
-// Loop through all numbers from 1 to 100
-// console.log("\n>>>>>>>>>>>>>>>>Loop through all numbers from 1 to 100<<<<<<<<<<<<<<<<<<<");
-// for( let i = 1 ; i <= 100; i++){
-//     console.log(i);
-// }
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
 
-//If a number is divisible by 3, log “Fizz.”
-console.log("\n>>>>>>>>>>>>>>>>If a number is divisible by 3, log “Fizz<<<<<<<<<<<<<<<<<<<");
 for( let i = 1 ; i <= 100; i++){
-    if(i % 3 == 0){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("Fizz Buzz");
+    }
+    else if(i % 3 == 0){
         console.log("Fizz")
     }
     else if(i % 5 == 0){
         console.log("Buzz");
     }
-    else if (i % 3 == 0 && i % 5 == 0){
-        console.log("Fizz Buzz");
+   
+    else {
+        console.log(i);
     }
 }
