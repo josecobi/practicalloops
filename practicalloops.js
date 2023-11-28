1554b0dd8d885cdc44dad8e7e942139aba896232
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-28 14:35:08 -0500

    If a number is divisible by 3 and 5, log <<Fizz Buzz>>

--- practicalloops.js
+++ practicalloops.js
@@ -13,5 +13,7 @@ for( let i = 1 ; i <= 100; i++){
     else if(i % 5 == 0){
         console.log("Buzz");
     }
-    else if (i % 3 == 0 && i % 5 == 0)
+    else if (i % 3 == 0 && i % 5 == 0){
+        console.log("Fizz Buzz");
+    }
 }
