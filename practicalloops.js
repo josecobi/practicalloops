3684293b19160e7e6de1517a2221194dba30a9e3
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-28 14:34:05 -0500

    If a number is divisible by 3, log <<Fizz>> if divisible by 5, log <<Buzz>>

--- practicalloops.js
+++ practicalloops.js
@@ -1,5 +1,17 @@
 // Loop through all numbers from 1 to 100
-console.log("\n>>>>>>>>>>>>>>>>Loop through all numbers from 1 to 100<<<<<<<<<<<<<<<<<<<");
+// console.log("\n>>>>>>>>>>>>>>>>Loop through all numbers from 1 to 100<<<<<<<<<<<<<<<<<<<");
+// for( let i = 1 ; i <= 100; i++){
+//     console.log(i);
+// }
+
+//If a number is divisible by 3, log “Fizz.”
+console.log("\n>>>>>>>>>>>>>>>>If a number is divisible by 3, log “Fizz<<<<<<<<<<<<<<<<<<<");
 for( let i = 1 ; i <= 100; i++){
-    console.log(i);
+    if(i % 3 == 0){
+        console.log("Fizz")
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz");
+    }
+    else if (i % 3 == 0 && i % 5 == 0)
 }
