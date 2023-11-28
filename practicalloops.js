7197aa2dff494c7e503a59ffea168b337e66ff2c
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-28 14:27:44 -0500

    Loop through all numbers from 1 to 100.

---
+++ practicalloops.js
@@ -0,0 +1,5 @@
+// Loop through all numbers from 1 to 100
+console.log("\n>>>>>>>>>>>>>>>>Loop through all numbers from 1 to 100<<<<<<<<<<<<<<<<<<<");
+for( let i = 1 ; i <= 100; i++){
+    console.log(i);
+}
